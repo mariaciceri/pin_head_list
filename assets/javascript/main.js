4e4bb0a6c1f9e065fbd104e19ed9292d6c39b6f2
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-10-09 17:04:39 +0000

    start with logic for adding a new list

---
+++ assets/javascript/main.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    let main = document.getElementsByTagName("main")[0];
+
+    main.addEventListener("click", () => {
+
+    })
+})
+
+
+
+
+
+//display the stored lists
+//  - list name; icon for the category (?)
+
+//when a list is opened:
+//  - name on top with icon
+//  - product name, price, where to buy;
+
+//display new form
+//  - list name(req); category; 
+//  - button to create
+
+//after creating the list:
+//  - click on the body to add a new item: name; price; where to buy;
+//  - show item on the screen with a checkbox; (gradient effect, fade away)
+
