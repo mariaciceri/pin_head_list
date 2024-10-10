2f9cb21e7732f45a02f4c44e0ceca39efa6e5c6c
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-10-10 10:20:40 +0000

    set up Firebase to store lists

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,28 @@
+// Import the functions from the SDKs 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { setDoc, doc, getDoc, getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAPvm9RW7ABgg2u8ZX4FjQFN5moWOHoxbg",
+    authDomain: "pin-8e7c8.firebaseapp.com",
+    projectId: "pin-8e7c8",
+    storageBucket: "pin-8e7c8.appspot.com",
+    messagingSenderId: "463561637941",
+    appId: "1:463561637941:web:5d71e7900e1b80b517b17c",
+    measurementId: "G-7BFSEETLVC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+/**
+ * Show popup window for creating a new list
+ */
 function createNewList() {
     const newButton = document.getElementById("popup"); //New button
     const closeCreateList = document.getElementById("close-button");//close button
@@ -13,32 +38,34 @@ function createNewList() {
     })
 };
 
+/**
+ * Show popup window for creating a new item
+ */
+//not working
+// function createNewItem() {
+//     const main = document.getElementById("main"); //click anywhere to open create item popup
+//     const closeCreateItem = document.getElementById("close-button-item");//close button
+//     const createItem = document.getElementById("create-item");//get the aside element that contains the create new item form;
 
-function createNewItem() {
-    const main = document.getElementById("main"); //click anywhere to open create item popup
-    const closeCreateItem = document.getElementById("close-button-item");//close button
-    const createItem = document.getElementById("create-item");//get the aside element that contains the create new item form;
+//     console.log(main)
+//     main.addEventListener("click", (event) => {
+//         const close = event.target === closeCreateItem;
+//         const createItemWindow = createItem.contains(event.target);
 
-    console.log(main)
-    main.addEventListener("click", (event) => {
-        const close = event.target === closeCreateItem;
-        const createItemWindow = createItem.contains(event.target);
+//         //check if the click is not on the closing button or inside the popup
+//         if (!close && !createItemWindow) {
+//             createItem.style.display = "block"; // Show the popup
+//         }
+//     });
 
-        //check if the click is not on the closing button or inside the popup
-        if (!close && !createItemWindow) {
-            createItem.style.display = "block"; // Show the popup
-        }
-    });
+//     closeCreateItem.addEventListener("click", () => {
+//         createItem.style.display = "none";
+//     });
+// }
 
-    closeCreateItem.addEventListener("click", () => {
-        createItem.style.display = "none";
-    });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    
+document.addEventListener("DOMContentLoaded", () => {   
     createNewList();
-    createNewItem();
+    //createNewItem();
 })
 
 
