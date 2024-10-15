8e16da9b94452c2b535cc10740886f470987c5bf
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-10-15 08:36:56 +0000

    change logic for when the user click on the save button

--- assets/javascript/my_list_manager.js
+++ assets/javascript/my_list_manager.js
@@ -8,7 +8,9 @@ export class ListManager {
         //retrieves what is saved already or creates an empty object;
         const lists = JSON.parse(localStorage.getItem('shoppingLists')) || {};
         //add the new list on the storage;
-        lists[listName] = storedItems;
+        lists[listName] = {
+            storedItems: storedItems,
+            listVisible: true };
         //saves the new list on the storage;
         localStorage.setItem('shoppingLists', JSON.stringify(lists))
         console.log("Data saved successfully in localStorage!");
@@ -18,7 +20,7 @@ export class ListManager {
     _retrieveListByName(listName, storedItems) {
         try {
             const storedList = JSON.parse(localStorage.getItem('shoppingLists'));
-            storedItems = storedList[listName];
+            storedItems = storedList[listName].storedItems;
             return storedItems;
         }
         catch (error) {
